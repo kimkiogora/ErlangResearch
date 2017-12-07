85bf0c019d17aa2e0bfdc4bcafde3f3364b94d1e
kiogora <kiogora.ikunyoah@roamtech.com>
2017-12-07 11:37:37 +0300

    Updated php test scripts

--- CRB/tests/TestCheckCRB.php
+++ CRB/tests/TestCheckCRB.php
@@ -1,13 +1,9 @@
 <?php
 require_once "Utils.php";
 
-#TestCase 1: Max Deposit per Transaction 
 echo "\n\n";
-//254707188026 |  24633429 
-$data_string = json_encode($payload, JSON_PRETTY_PRINT);
-#die("$data_string\n\n");
-$url = 'http://localhost:9002/check?msisdn=254711240985';
-$resp = send_post_request( $url, $data_string,'GET');
+$url = 'http://localhost:9002/check?contact=254711240985';
+$resp = send_post_request( $url, NULL,'GET');
 $resp = json_encode($resp,JSON_PRETTY_PRINT);
 print_r($resp);
 
